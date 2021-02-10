f988a50e544837b893e2fb7b7b3941295dcb3d5a
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-02-09 22:31:49 -0300

    Create 1001_Basico.py

---
+++ 1001_Basico.py
@@ -0,0 +1,11 @@
+"""
+	@autor: Manrick Leal;
+	@data: 09/02/2021;
+	@nome: Extremamente Básico 1001 URI;
+"""
+# leitura de A e B
+value1 = int(input())
+value2 = int(input())
+
+# Exibindo na Tela a soma de A + B
+print('X = {}'.format(value1+value2))
